6e9d971b418da57169d870a245c703903ac3d508
tiagodbarbieri <tiagodbarbieri@hotmail.com>
2025-03-28 11:17:05 -0300

    Adicionado usuário admin

--- prj_vitrine_virtual/main/admin.py
+++ prj_vitrine_virtual/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Item, Image
+
+# Dados para login:
+#   user: admin_ibj
+#   password: ibj_2025
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Image)
