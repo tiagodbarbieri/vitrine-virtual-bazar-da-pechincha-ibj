0f939e3a5fcf63b65930f32dc97ccd36c9a7123a
João Toldo <joao.toldo@protonmail.com>
2025-03-27 21:15:12 -0300

    interface adimin

--- prj_vitrine_virtual/main/admin.py
+++ prj_vitrine_virtual/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+# importa os modelos de models.py
+from .models import Category
+from .models import Item
+from .models import Image
+
+# add cada modelo(tabela na interface admin
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Image)
