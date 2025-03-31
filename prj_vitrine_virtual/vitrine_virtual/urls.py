27801700ea7862547d7b31bc7e63d4dc8930adb1
Jonatas <jonatasmfarias@gmail.com>
2025-03-31 16:26:28 -0300

    Substituído a view listar_itens pelo TemplateView na URL da página inicial

--- prj_vitrine_virtual/vitrine_virtual/urls.py
+++ prj_vitrine_virtual/vitrine_virtual/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from main.views import listar_itens  # Importe a view listar_itens
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", listar_itens, name="index"),  # Substitua o TemplateView pela view listar_itens
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("quem-somos/", TemplateView.as_view(template_name="quem_somos.html"), name="quem-somos"),
     path("contato/", TemplateView.as_view(template_name="contato.html"), name="contato"),
     path("admin/", admin.site.urls),
