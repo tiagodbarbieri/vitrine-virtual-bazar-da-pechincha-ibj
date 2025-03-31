e6ff083a0d244a35794024432140aafc7660cab8
Jonatas <jonatasmfarias@gmail.com>
2025-03-31 16:11:30 -0300

    Adicionada funcionalidade para listar itens disponíveis na página inicial

--- prj_vitrine_virtual/main/views.py
+++ prj_vitrine_virtual/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+def listar_itens(request):
+    itens = Item.objects.all()  # Busca todos os objetos da classe Item
+    print(itens)
+    return render(request, "index.html", {"itens": itens})  # Passa os itens para o template
