722eff7632555e62432484110248cfef098e3b18
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2025-03-31 21:55:11 -0300

    Removido duplas importações

--- prj_vitrine_virtual/main/admin.py
+++ prj_vitrine_virtual/main/admin.py
@@ -6,12 +6,7 @@ from .models import Category, Item, Image
 #   password: ibj_2025
 
 # Register your models here.
-# importa os modelos de models.py
-from .models import Category
-from .models import Item
-from .models import Image
-
-# add cada modelo(tabela na interface admin
+# Adiciona cada modelo/tabela na interface admin
 admin.site.register(Category)
 admin.site.register(Item)
 admin.site.register(Image)
