2508bc0c5bfe080936828864eac24a72c0f3aae7
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2025-04-01 12:22:14 -0300

    Adicionado método include no urlpatterns

--- prj_vitrine_virtual/vitrine_virtual/urls.py
+++ prj_vitrine_virtual/vitrine_virtual/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
-from main.views import listar_itens
+
 
 urlpatterns = [
-    path("", listar_itens, name="index"),
+    path("", include("main.urls")),
     path("quem-somos/", TemplateView.as_view(template_name="quem_somos.html"), name="quem-somos"),
     path("contato/", TemplateView.as_view(template_name="contato.html"), name="contato"),
     path("admin/", admin.site.urls),
