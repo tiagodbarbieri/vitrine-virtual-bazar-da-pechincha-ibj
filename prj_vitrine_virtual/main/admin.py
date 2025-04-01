ae0c6b1df967c035bcf5fff5f731a9c9bbbd44d6
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2025-03-31 22:07:44 -0300

    Atualizado usuário e senha

--- prj_vitrine_virtual/main/admin.py
+++ prj_vitrine_virtual/main/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Category, Item, Image
 
 # Dados para login:
-#   user: admin_ibj
-#   password: ibj_2025
+#   user: admin
+#   password: admin123
 
 # Register your models here.
 # Adiciona cada modelo/tabela na interface admin
