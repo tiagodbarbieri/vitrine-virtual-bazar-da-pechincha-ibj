25172927e8b915ddaa6e54c23245245a058a09a3
tiagodbarbieri <tiagodbarbieri@hotmail.com>
2025-04-02 18:01:19 -0300

    Tentado implementar pesquisa...

--- prj_vitrine_virtual/main/forms.py
+++ prj_vitrine_virtual/main/forms.py
@@ -10,7 +10,7 @@ def categories():
 
 
 class Search(forms.Form):
-    order_list = [("1", "por nome"), ("2", "mais recentes"), ("3", "menor valor"), ("4", "maior valor")]
-    category = forms.ChoiceField(choices=categories(), label="Categoria")
-    word = forms.CharField(required=False, label="Palavra chave:", initial="Digite...")
-    order = forms.ChoiceField(choices=order_list, label="Ordenar por:")
+    order_list = [("1", "nome"), ("2", "mais recentes"), ("3", "menor valor"), ("4", "maior valor")]
+    category = forms.ChoiceField(choices=categories())
+    word = forms.CharField(required=False, initial="Digite...")
+    order = forms.ChoiceField(choices=order_list)
