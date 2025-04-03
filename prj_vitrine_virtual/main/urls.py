f733bf41868545021dcb2fbf922e6c7c06aaf56a
OsmarMSantos <osmaranchieta@gmail.com>
2025-04-03 20:20:39 -0300

    Inclusão da tela de Detalhes  porém apos a criação da tela principal preciso alterar a forma de apresentação do item para recuperar informações do banco

--- prj_vitrine_virtual/main/urls.py
+++ prj_vitrine_virtual/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from main.views import detalhe
 
 urlpatterns = [
     path("", views.listar_itens, name="index"),
+    path('detalhe/', detalhe, name='detalhe'),
 ]
--- prj_vitrine_virtual/main/views.py
+++ prj_vitrine_virtual/main/views.py
@@ -4,3 +4,5 @@ from .models import Item
 def listar_itens(request):
     lista_itens = Item.objects.all()  # Obtém todos os itens
     return render(request, "index.html", {"lista_itens": lista_itens})
+def detalhe(request):
+    return render(request, 'detalhe.html')
