715fcf9e14816d9a752162a2ad1e70c7f8fd4522
Jonatas <jonatasmfarias@gmail.com>
2025-04-03 01:38:32 -0300

    Adicionar suporte para filtragem de itens por categoria na URL

--- prj_vitrine_virtual/main/urls.py
+++ prj_vitrine_virtual/main/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
-    path("", views.listar_itens, name="index"),
+    path("", views.index, name="index"),
+    path("categoria/<slug:slug_categoria>/", views.index, name="filtrar_por_categoria"),
 ]
