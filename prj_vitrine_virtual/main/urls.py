023e031166ee7f65db4e9a170af34861799a0042
Jonatas <jonatasmfarias@gmail.com>
2025-04-03 01:48:40 -0300

    Remover o namespace 'app_name' e a rota de filtragem por categoria

--- prj_vitrine_virtual/main/urls.py
+++ prj_vitrine_virtual/main/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 
 from . import views
 
-app_name = 'main'
-
 urlpatterns = [
     path("", views.index, name="index"),
-    path("categoria/<slug:slug_categoria>/", views.index, name="filtrar_por_categoria"),
 ]
