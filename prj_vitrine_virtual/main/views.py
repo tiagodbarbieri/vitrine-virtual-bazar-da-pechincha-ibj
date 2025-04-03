15ec132bdf014a5c80eb98ede49983db575f1979
Jonatas <jonatasmfarias@gmail.com>
2025-04-03 01:31:25 -0300

    Adicionar funcionalidade de busca por nome e refatorar a listagem de itens na página inicial

--- prj_vitrine_virtual/main/views.py
+++ prj_vitrine_virtual/main/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
-
 from .models import Category, Item
 
-
-def listar_itens (request, slug_categoria=None):
+def index(request, slug_categoria=None):
     categoria = None
-    lista_categorias = Category.objects.all()
-    lista_itens = Item.objects.filter(disponivel=True)
+    lista_categorias = Category.objects.all()  # Todas as categorias
+    lista_itens = Item.objects.filter(disponivel=True)  # Itens disponíveis
+
+    # Filtro por categoria
     if slug_categoria:
         categoria = get_object_or_404(Category, slug=slug_categoria)
-        lista_itens = Item.objects.filter(categoria=categoria) 
-    contexto = {'categoria': categoria,'lista_categorias': lista_categorias,'lista_itens': lista_itens}
-    return render(request, "item/listar.html",contexto)
-def detalhes_item(request, id, slug):
-    item = get_object_or_404(Item, id=id, slug=slug, disponivel=True)
-    contexto = {'item': item}
-    return render(request, "item/detalhes.html", contexto) 
+        lista_itens = lista_itens.filter(categoria=categoria)
+
+    # Busca por nome do item
+    search_query = request.GET.get('search', '')
+    if search_query:
+        lista_itens = lista_itens.filter(name__icontains=search_query)
+
+    contexto = {
+        'categoria': categoria,
+        'lista_categorias': lista_categorias,
+        'lista_itens': lista_itens,
+        'search_query': search_query,
+    }
+    return render(request, "index.html", contexto)
