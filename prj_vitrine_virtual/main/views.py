9e45466134dc1aac86111995d14dfe9d6f1a12ab
tiagodbarbieri <tiagodbarbieri@hotmail.com>
2025-04-04 17:33:01 -0300

    Atualizado função home

--- prj_vitrine_virtual/main/views.py
+++ prj_vitrine_virtual/main/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.views.generic.edit import FormView
-from main import forms
+from .forms import Search
 
 
-# Create your views here.
-class Home(FormView):
-    template_name = "home.html"
-    form_class = forms.Search
-    success_url = "/"
+def home(request):
+    if request.method == "GET":
+        # Buscar todos os itens e listá-los por mais recentes...
+        # Mostrar por página no máximo 16 items
+
+        return render(request, "home.html", {"form": Search})
+
+    elif request.method == "POST":
+        # Capturando os dados enviados
+        form = Search(request.POST)
+
+        # Verificando se os dados são válidos
+        if form.is_valid():
+            category = form.cleaned_data["category"]
+            word = form.cleaned_data["word"]
+            order = form.cleaned_data["order"]
+
+            # Buscar os itens no banco de dados conforme categoria e palavra chave
+            # Ordenar os itens
+
+            print(category)
+            print(word)
+            print(order)
+
+        return render(request, "home.html", {"form": form})
