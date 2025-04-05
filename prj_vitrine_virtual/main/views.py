fcaf1dd6df8447508aac082e930a0ea19c5095c1
Jonatas <jonatasmfarias@gmail.com>
2025-04-05 02:34:49 -0300

    Adicionar filtro de categoria e estilização ao formulário de busca na lista de produtos

--- prj_vitrine_virtual/main/views.py
+++ prj_vitrine_virtual/main/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from .models import Item
+from .models import Item, Category
 
 def listar_itens(request):
-    busca = request.GET.get('busca', '')
+    busca = request.GET.get('busca', '').strip()
+    categoria = request.GET.get('categoria', '').strip()
+    lista_itens = Item.objects.all()
+    
     if busca:
-        lista_itens = Item.objects.filter(description__icontains=busca)
-    else:
-        lista_itens = Item.objects.all()
-    return render(request, "index.html", {"lista_itens": lista_itens})
+        lista_itens = lista_itens.filter(description__icontains=busca)
+    
+    if categoria:
+        lista_itens = lista_itens.filter(category__name=categoria)
+    
+    # Consulta as categorias que deseja exibir (ajuste os filtros se necessário)
+    categories = Category.objects.filter(status=True).order_by('name')
+        
+    return render(request, "index.html", {
+        "lista_itens": lista_itens,
+        "categories": categories
+    })
