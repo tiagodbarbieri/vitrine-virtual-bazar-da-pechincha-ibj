1d5608f1e73ddeaec9741b90ec8adfb10448005c
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2025-04-04 22:05:59 -0300

    Adicionado mapeamento para carregar as imagens da pasta upload

--- prj_vitrine_virtual/main/urls.py
+++ prj_vitrine_virtual/main/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from . import views
 from main.views import detalhe
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.listar_itens, name="index"),
-    path('detalhe/', detalhe, name='detalhe'),
-]
+    path("detalhe/<int:item_id>", detalhe, name="detalhe"),
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)  # Mapeamento para carregar as imagens da pasta upload
--- prj_vitrine_virtual/vitrine_virtual/settings.py
+++ prj_vitrine_virtual/vitrine_virtual/settings.py
@@ -119,10 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "upload"),  # Permite carregar arquivos de upload/
-]
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
