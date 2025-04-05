0879dc48c6db3257bba4eff6298a497e206389fd
Jonatas <jonatasmfarias@gmail.com>
2025-04-05 02:25:10 -0300

    Adicionar funcionalidade de busca e estilização ao formulário de pesquisa na lista de produtos

--- prj_vitrine_virtual/main/views.py
+++ prj_vitrine_virtual/main/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from .models import Item
 
 def listar_itens(request):
-    lista_itens = Item.objects.all()  # Obtém todos os itens
+    busca = request.GET.get('busca', '')
+    if busca:
+        lista_itens = Item.objects.filter(description__icontains=busca)
+    else:
+        lista_itens = Item.objects.all()
     return render(request, "index.html", {"lista_itens": lista_itens})
