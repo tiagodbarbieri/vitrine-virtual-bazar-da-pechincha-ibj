11e3f6cd9dbf196b9e5841f46087abc883f88425
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2025-04-04 22:09:02 -0300

    Adicionado funções para buscar itens no DB

--- prj_vitrine_virtual/main/views.py
+++ prj_vitrine_virtual/main/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-from .models import Item
+from django.shortcuts import render, get_object_or_404
+from .models import Item, Image
+
 
 def listar_itens(request):
     lista_itens = Item.objects.all()  # Obtém todos os itens
     return render(request, "index.html", {"lista_itens": lista_itens})
-def detalhe(request):
-    return render(request, 'detalhe.html')
+
+
+def detalhe(request, item_id):
+    item = get_object_or_404(Item, id=item_id)  # Obtém os itens com o respectivo id
+    images = Image.objects.filter(item_id=item)  # Obtém a lista de imagens referentes ao respectivo item
+    return render(request, "detalhe.html", {"item": item, "images": images})
