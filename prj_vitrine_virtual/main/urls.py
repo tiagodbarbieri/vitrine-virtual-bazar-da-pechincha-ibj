7302c68dba5e4c2d521b7a52d1f57217b1bf24ff
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2025-04-09 23:22:40 -0300

    Trocado identificação do item de id para o slug no path da página de detalhe do item

--- prj_vitrine_virtual/main/urls.py
+++ prj_vitrine_virtual/main/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 
 from . import views
 from main.views import detalhe
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path("", views.listar_itens, name="index"),
-    path("detalhe/<int:item_id>", detalhe, name="detalhe"),
-] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)  # Mapeamento para carregar as imagens da pasta upload
+    path("", views.home, name="home-page"),
+    path("detalhe/<str:slug>", detalhe, name="detalhe"),
+]
--- prj_vitrine_virtual/main/views.py
+++ prj_vitrine_virtual/main/views.py
@@ -59,7 +59,7 @@ def listar_itens(request):
     return render(request, "index.html", {"lista_itens": lista_itens})
 
 
-def detalhe(request, item_id):
-    item = get_object_or_404(Item, id=item_id)  # Obtém os itens com o respectivo id
+def detalhe(request, slug):
+    item = get_object_or_404(Item, slug=slug)  # Obtém os itens com o respectivo id
     images = Image.objects.filter(item_id=item)  # Obtém a lista de imagens referentes ao respectivo item
     return render(request, "detalhe.html", {"item": item, "images": images})
