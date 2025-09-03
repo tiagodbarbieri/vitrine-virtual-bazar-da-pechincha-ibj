700fea131930f3f043feec270012a18aa45bf282
tiagodbarbieri <tiagodbarbieri@hotmail.com>
2025-09-03 12:49:09 -0300

    Refactor.

--- prj_vitrine_virtual/main/urls.py
+++ prj_vitrine_virtual/main/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from main.views import home
 from main.views import detalhe
 
 
 urlpatterns = [
-    path("", views.home, name="home-page"),
+    path("", home, name="home-page"),
     path("detalhe/<str:slug>", detalhe, name="detalhe"),
 ]
