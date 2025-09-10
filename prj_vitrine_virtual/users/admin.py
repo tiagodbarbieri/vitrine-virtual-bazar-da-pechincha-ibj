cb7e2ca7ba0e0737d3e91a24b984ad5467ff19b9
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2025-09-09 22:28:31 -0300

    Adicionado número de telefone ao usuário através da técnica de um para um.

--- prj_vitrine_virtual/users/admin.py
+++ prj_vitrine_virtual/users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from users.models import UserInfo
+
+
+# Define an inline admin descriptor for UserInfo model
+# which acts a bit like a singleton
+class UserInfoInline(admin.StackedInline):
+    model = UserInfo
+    can_delete = False
+    verbose_name = "Informação adicional"
+    verbose_name_plural = "Informações adicionais"
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    def phone_number(self, obj):
+        user_info = UserInfo.objects.get(user=obj.id)
+        if hasattr(user_info, "phone_number"):
+            return user_info.phone_number
+        else:
+            return "-"
+
+    phone_number.short_description = "Telefone"
+    list_display = BaseUserAdmin.list_display + ("phone_number",)
+    inlines = [UserInfoInline]
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- prj_vitrine_virtual/users/models.py
+++ prj_vitrine_virtual/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Estendendo o modelo Usuário existente
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
