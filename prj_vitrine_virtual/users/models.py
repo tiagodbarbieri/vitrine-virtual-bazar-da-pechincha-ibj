080943d760047f055a1ededc520b745a092b4e4f
tiagodbarbieri <tiagodbarbieri@hotmail.com>
2025-09-10 16:48:55 -0300

    Adicionado outras informações para o usuário.

--- prj_vitrine_virtual/users/models.py
+++ prj_vitrine_virtual/users/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Estendendo o modelo Usuário existente
+GENDER = [("M", "Masculino"), ("F", "Feminino"), ("O", "Outro")]
+
+
+# Estendendo o modelo do Usuário existente
 class UserInfo(models.Model):
+    cpf = models.CharField(max_length=14)
+    birthday = models.DateField()
+    gender = models.CharField(max_length=1, choices=GENDER, default="M")
+    phone_number = models.CharField(max_length=20)
+    privacy_police = models.BooleanField(default=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15)
