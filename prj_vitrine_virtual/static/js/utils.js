4d101f0b3acd3c644b17a71cbdc77a695c0b6d45
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2025-09-18 22:20:11 -0300

    Continuação da funcionalidade de reservar itens.

--- prj_vitrine_virtual/static/js/utils.js
+++ prj_vitrine_virtual/static/js/utils.js
@@ -1,6 +1,7 @@
 // Impede que o clique feche o dropdown
 {
-    document.getElementById("submit-btn").addEventListener("click", function(e){
+    const submit_button = document.getElementById("submit-btn");
+    submit_button.addEventListener("click", function(e){
         e.stopPropagation();
     });
 }
@@ -15,11 +16,10 @@
     dropdown_form.addEventListener("submit", async function(event){
         event.preventDefault(); // Impede o envio tradicional do formulário
         const formData = new FormData(this); // Formulário
-        const token = document.getElementsByName("csrfmiddlewaretoken").values(); // Token CSRF exigido pelo Django
         const response = await fetch('/users/login/', {
             method: 'POST',
             headers: {
-                'X-CSRFToken': token // Inserção do token no cabeçalho
+                'X-CSRFToken': getCookie('csrftoken') // Inserção do token no cabeçalho
             },
             body: formData
         });
@@ -38,7 +38,7 @@
 
 // Função para confirmação de exclusão de conta
 function delete_account(){
-    if (confirm("Deseja realmente excluir sua conta?") == true) {
+    if (window.confirm("Deseja realmente excluir sua conta?") == true) {
         // Chamar a função de exclusão de conta
         window.location.href = '/users/excluir-conta/';
     }
@@ -46,10 +46,60 @@ function delete_account(){
 
 // Função que solicita a reserva do item ao servidor
 function reserve_item(item_id, total_items){
-    console.log(`Botão clicado! ID=${item_id}, QTS=${total_items}`);
+    const input = document.getElementById("items_quantity");
+    const item_qty = Number(input.value);
+    let option = false;
+
+    if (item_qty >= 1 && item_qty <= Number(total_items)){
+        option = window.confirm(`Você selecionou a quantidade de ${item_qty} itens, deseja reservá-los?`);
+    } else if (item_qty > Number(total_items)){
+        window.alert(`Você deve selecionar no máximo a quantidade de ${total_items} itens!`);
+    } else {
+        window.alert("Você deve escolher a quantidade de um item ou mais!");
+    };
+
+    if (option){
+        const response = fetch("/users/reservar-item/", {
+            method: "POST",
+            headers: {
+                'X-CSRFToken': getCookie('csrftoken'), // Inserção do token no cabeçalho
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                item_id: item_id,
+                item_qty: item_qty
+            })
+        }).then(response => {
+            if (!response.ok){
+                throw new Error("Erro na resposta do servidor");
+            };
+            return response.json(); // Converte a resposta pa JS
+        }).then(data => {
+            console.log("Resposta do Django:", data);
+        }).catch(error => {
+            console.error("Erro:", error);
+        })
+    };
 }
 
 // Função que mostra um popover informando que é necessário fazer login
 function show_popover(){
     console.log("O mouse entrou no botão!");
+}
+
+// Função que o token do Django
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
