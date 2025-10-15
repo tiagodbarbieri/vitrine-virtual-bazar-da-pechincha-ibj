b1b0eb5bf5bf0637e173f22e058a7d98bcf60b4a
tiagodbarbieri <tiagodbarbieri@hotmail.com>
2025-10-15 18:31:26 -0300

    Implementado testes para o forms do users.

---
+++ prj_vitrine_virtual/users/tests/tests_forms.py
@@ -0,0 +1,106 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from unittest.mock import patch
+from users.models import UserInfo
+from users.forms import Register, Update
+
+
+class TestarRegister(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="maria",
+            password="maria@9874#",
+            email="maria.silva@gmail.com",
+        )
+        self.user_info = UserInfo.objects.create(
+            cpf="754.896.384-00",
+            birthday="1980-02-01",
+            gender="F",
+            phone_number="(11)91234-5678",
+            privacy_police=True,
+            user=self.user,
+        )
+
+    @patch("users.forms.ReCaptchaField.clean")
+    def testar_criacao_de_usuario_valido(self, mock_clean):
+        mock_clean.return_value = None  # Simula CAPTCHA válido
+
+        dados = {
+            "username": "joao",
+            "email": "joao.silva@gmail.com",
+            "password": "joao@9874#",
+            "password_confirmation": "joao@9874#",
+            "first_name": "Joao",
+            "last_name": "Silva",
+            "cpf": "092.274.019-46",
+            "birthday": "1980-02-01",
+            "phone_number": "(11)91234-5678",
+            "gender": "M",
+            "privacy_police": "on",
+            "g-recaptcha-response": "fake-response",
+        }
+        form = Register(data=dados)
+        self.assertTrue(form.is_valid())
+
+    @patch("users.forms.ReCaptchaField.clean")
+    def testar_criacao_de_usuario_invalido(self, mock_clean):
+        mock_clean.return_value = None  # Simula CAPTCHA válido
+
+        dados = {
+            "username": "maria",
+            "email": "maria.silva@gmail.com",
+            "password": "maria@9874#",
+            "password_confirmation": "maria@9874",
+            "first_name": "Maria",
+            "last_name": "Silva",
+            "cpf": "754.896.384-00",
+            "birthday": "1980-02-01",
+            "phone_number": "(11)91234-56781",
+            "gender": "F",
+            "privacy_police": "on",
+            "g-recaptcha-response": "fake-response",
+        }
+        form = Register(data=dados)
+        self.assertFalse(form.is_valid())
+        self.assertIn('O nome de usuário "maria" já está cadastrado!', form.errors["username"])
+        self.assertIn('O e-mail "maria.silva@gmail.com" já está cadastrado!', form.errors["email"])
+        self.assertIn("As senhas digitadas não coincidem!", form.errors["password_confirmation"])
+        self.assertIn("Esse CPF já está cadastrado!", form.errors["cpf"])
+        self.assertIn("O número de celular informado é invalido! informe o DDD + número.", form.errors["phone_number"])
+
+
+class TestarUpdate(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="maria",
+            password="maria@9874#",
+            email="maria.silva@gmail.com",
+        )
+        self.user_info = UserInfo.objects.create(
+            cpf="754.896.384-00",
+            birthday="1980-02-01",
+            gender="F",
+            phone_number="(11)91234-5678",
+            privacy_police=True,
+            user=self.user,
+        )
+
+    def testar_atualizacao_de_dados_validos(self):
+        dados = {
+            "username": "maria",
+            "email": "maria.silva@gmail.com",
+            "password": "maria@9874#",
+            "password_confirmation": "maria@9874#",
+            "first_name": "Maria",
+            "last_name": "Silva",
+            "cpf": "754.896.384-00",
+            "birthday": "1980-02-01",
+            "phone_number": "(11)91234-5678",
+            "gender": "F",
+            "privacy_police": "on",
+        }
+        form = Update(user_logged=self.user, data=dados)
+        self.assertTrue(form.is_valid())
+
+    def testar_atualizacao_de_dados_invalidos(self):
+        pass
