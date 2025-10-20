422f55d9b1cfc8afa173e89ee9a0afbb42779ac8
tiagodbarbieri <tiagodbarbieri@hotmail.com>
2025-10-20 18:02:18 -0300

    Adicionado mais alguns testes.

--- prj_vitrine_virtual/users/tests/tests_views.py
+++ prj_vitrine_virtual/users/tests/tests_views.py
@@ -1,31 +1,87 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
-from users.views import cadastro
-from users.views import redefinir_senha
-from users.views import minha_conta
-from users.views import minhas_reservas
-from users.views import reservar_item
-from users.views import apagar_reserva
-from users.views import login
-from users.views import logout
-from users.views import excluir_conta
+from django.urls import reverse
+from unittest.mock import patch
+
+from users.models import UserInfo
+
+# from users.views import cadastro
+# from users.views import redefinir_senha
+# from users.views import minha_conta
+# from users.views import minhas_reservas
+# from users.views import reservar_item
+# from users.views import apagar_reserva
+# from users.views import login
+# from users.views import logout
+# from users.views import excluir_conta
 
 
 class TestarPaginaCadastro(TestCase):
     def testar_se_cadastro_carrega_completamente(self):
-        pass
+        response = self.client.get(reverse("cadastro"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "cadastro.html")
+        self.assertContains(response, "CADASTRO")
 
-    def testar_se_o_cadastro_tem_sucesso(self):
-        pass
+    @patch("users.forms.ReCaptchaField.clean")
+    def testar_se_o_cadastro_tem_sucesso(self, mock_clean):
+        mock_clean.return_value = None  # Simula CAPTCHA válido
+
+        data = {
+            "username": "maria",
+            "email": "maria.silva@gmail.com",
+            "password": "maria@9874#",
+            "password_confirmation": "maria@9874",
+            "first_name": "Maria",
+            "last_name": "Silva",
+            "cpf": "754.896.384-00",
+            "birthday": "1980-02-01",
+            "phone_number": "(11)91234-56781",
+            "gender": "F",
+            "privacy_police": "on",
+            "g-recaptcha-response": "fake-response",
+        }
+        response = self.client.post(reverse("cadastro"), data)  # follow=True
+
+        self.assertEqual(response.status_code, 200)
+        # Para que os testes abaixo funcionem, será necessário criar uma nova view para cadastro finalizado
+        # com gerenciamento de seção --> "request.session".
+
+        # self.assertTemplateUsed(response, "cadastro_finalizado.html")
+        # self.assertContains(response, "CADASTRO FINALIZADO")
+        # self.assertContains(response, "Maria")
 
 
 class TestarPaginaRedefinirSenha(TestCase):
     def testar_se_redefinir_senha_carrega_completamente(self):
-        pass
+        response = self.client.get(reverse("redefinir-senha"))
+        self.assertAlmostEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "redefinir_senha.html")
+        self.assertContains(response, "REDEFINIR SENHA")
 
 
 class TestarPaginaMinhaConta(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="maria",
+            password="maria@9874#",
+            email="maria.silva@gmail.com",
+        )
+        self.user_info = UserInfo.objects.create(
+            cpf="754.896.384-00",
+            birthday="1980-02-01",
+            gender="F",
+            phone_number="(11)91234-5678",
+            privacy_police=True,
+            user=self.user,
+        )
+        self.client.force_login(self.user)
+
     def testar_se_minha_conta_carrega_completamente(self):
-        pass
+        response = self.client.get(reverse("minha-conta"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "minha_conta.html")
+        self.assertContains(response, "maria")
 
     def testar_se_minha_conta_atualiza_dados(self):
         pass
