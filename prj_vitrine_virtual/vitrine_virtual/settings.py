e6a436dfa6c7ff53041ae5e9c248e1142e334e73
phobozz1 <recarvalho85@gmail.com>
2025-10-31 10:13:58 -0300

    Novo fix para acesso a variáveis de ambiente

--- prj_vitrine_virtual/vitrine_virtual/settings.py
+++ prj_vitrine_virtual/vitrine_virtual/settings.py
@@ -11,11 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Carrega variáveis de ambiente do arquivo .env - Fix porco até encontrar um jeito melhor
+load_dotenv("~/vitrinev2.env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
